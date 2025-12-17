ddbfcb78a19e4f0243e861e825e37c4d5595d83d
Mattdelgado <89465381+mattdlgado@users.noreply.github.com>
2025-12-17 13:20:36 +0100

    Remove svg-preview wrapper and render SVG directly with dangerouslySetInnerHTML

--- gutenberg-addons/blocks/svg-block/svg-block.js
+++ gutenberg-addons/blocks/svg-block/svg-block.js
@@ -1,75 +1,71 @@
 (function (blocks, element, blockEditor, components, i18n) {
-    var el = element.createElement;
-    var TextareaControl = components.TextareaControl;
-    var InspectorControls = blockEditor.InspectorControls;
-    var PanelBody = components.PanelBody;
-    var useBlockProps = blockEditor.useBlockProps;
-    var __ = i18n.__;
+	var el = element.createElement;
+	var registerBlockType = blocks.registerBlockType;
+	var InspectorControls = blockEditor.InspectorControls;
+	var useBlockProps = blockEditor.useBlockProps;
+	var PanelBody = components.PanelBody;
+	var TextareaControl = components.TextareaControl;
+	var __ = i18n.__;
 
-    blocks.registerBlockType('custom/svg-block', {
-        title: __('SVG', 'gutenberg-addons'),
-        description: __('Añade código SVG personalizado', 'gutenberg-addons'),
-        icon: 'art',
-        category: 'widgets',
-        supports: {
-            align: true,
-            html: false
-        },
-        attributes: {
-            svgCode: {
-                type: 'string',
-                default: '<svg width="100" height="100" xmlns="http://www.w3.org/2000/svg"><circle cx="50" cy="50" r="40" fill="#0073aa" /></svg>'
-            }
-        },
+	registerBlockType('gutenberg-addons/svg-block', {
+		title: __('SVG Block', 'gutenberg-addons'),
+		icon: 'art',
+		category: 'media',
+		attributes: {
+			svgCode: {
+				type: 'string',
+				default: ''
+			}
+		},
+		edit: function (props) {
+			var attributes = props.attributes;
+			var setAttributes = props.setAttributes;
 
-        edit: function (props) {
-            var attributes = props.attributes;
-            var setAttributes = props.setAttributes;
-            var blockProps = useBlockProps ? useBlockProps() : {};
+			function onChangeSVGCode(newCode) {
+				setAttributes({ svgCode: newCode });
+			}
 
-            function onChangeSVGCode(newCode) {
-                setAttributes({ svgCode: newCode });
-            }
+			var blockProps = useBlockProps();
 
-            return el(
-                'div',
-                blockProps,
-                el(InspectorControls, {},
-                    el(PanelBody, { title: __('Configuración SVG', 'gutenberg-addons') },
-                        el(TextareaControl, {
-                            label: __('Código SVG', 'gutenberg-addons'),
-                            help: __('Pega tu código SVG aquí', 'gutenberg-addons'),
-                            value: attributes.svgCode,
-                            onChange: onChangeSVGCode,
-                            rows: 10
-                        })
-                    )
-                ),
-                el('div', { className: 'svg-preview' },
-                    el('p', {}, el('strong', {}, __('Vista previa:', 'gutenberg-addons'))),
-                    el('div', {
-                        className: 'svg-preview-content',
-                        dangerouslySetInnerHTML: { __html: attributes.svgCode }
-                    })
-                )
-            );
-        },
+			return el(
+				'div',
+				blockProps,
+				el(
+					InspectorControls,
+					{},
+					el(
+						PanelBody,
+						{
+							title: __('Configuración SVG', 'gutenberg-addons')
+						},
+						el(TextareaControl, {
+							label: __('Código SVG', 'gutenberg-addons'),
+							help: __('Pega tu código SVG aquí', 'gutenberg-addons'),
+							value: attributes.svgCode,
+							onChange: onChangeSVGCode,
+							rows: 10
+						})
+					)
+				),
+				el('div', {
+					dangerouslySetInnerHTML: { __html: attributes.svgCode }
+				})
+			);
+		},
+		save: function (props) {
+			var attributes = props.attributes;
+			var blockProps = useBlockProps.save();
 
-        save: function (props) {
-            var blockProps = useBlockProps ? useBlockProps.save() : {};
-            
-            if (!props.attributes.svgCode) {
-                return null;
-            }
-
-            // Renderizar solo el SVG sin div contenedor
-            return el(element.RawHTML, blockProps, props.attributes.svgCode);
-        }
-    });
+			return el('div', {
+				...blockProps,
+				dangerouslySetInnerHTML: { __html: attributes.svgCode }
+			});
+		}
+	});
 })(
-    window.wp.blocks,
-    window.wp.element,
-    window.wp.blockEditor,
-    window.wp.components,
-    window.wp.i18n
+	window.wp.blocks,
+	window.wp.element,
+	window.wp.blockEditor,
+	window.wp.components,
+	window.wp.i18n
 );
